3b587793f67f86fb5dd40c9ebc4463b30fb26a92
Tej <tejmandaliya@gmail.com>
2024-01-31 19:04:26 +0530

    k

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-
+    // AndroidX and Kotlin dependencies
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -61,15 +61,24 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Firebase dependencies
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+
+    // Google Play services dependencies
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // Testing dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Lottie animation library
+    implementation("com.airbnb.android:lottie:6.3.0")
 }
--- app/src/main/java/com/example/helpvehicle/LoginActivity.kt
+++ app/src/main/java/com/example/helpvehicle/LoginActivity.kt
@@ -1,46 +1,152 @@
 package com.example.helpvehicle
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.appcompat.app.AlertDialog
 import com.example.helpvehicle.databinding.ActivityLoginBinding
-import com.example.helpvehicle.databinding.ActivitySignupBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-        binding.loginButton.setOnClickListener{
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        findViewById<Button>(R.id.Gbutton).setOnClickListener{`
+            signInGoogle()
+        }
+
+
+        binding.loginButton.setOnClickListener {
             val email = binding.loginEmail.text.toString()
-            val password  = binding.loginPassword.text.toString()
+            val password = binding.loginPassword.text.toString()
 
-            if (email.isNotEmpty() && password.isNotEmpty()){
-                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
-                    if (it.isSuccessful){
-                        val intent = Intent(this, MainActivity::class.java)
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, HomeActivity::class.java)
                         startActivity(intent)
-                    } else{
-                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                     }
                 }
-            } else{
+            } else {
                 Toast.makeText(this, "Fields cannot be Empty", Toast.LENGTH_SHORT).show()
             }
         }
-        binding.signupRedirectText.setOnClickListener{
+
+        binding.forgotPassword.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.dialog_forget, null)
+            val userEmail = view.findViewById<EditText>(R.id.editBox)
+
+            builder.setView(view)
+            val dialog = builder.create()
+
+            view.findViewById<Button>(R.id.btnReset).setOnClickListener {
+                compareEmail(userEmail)
+                dialog.dismiss()
+            }
+            view.findViewById<Button>(R.id.btnCancel).setOnClickListener {
+                dialog.dismiss()
+            }
+            if (dialog.window != null) {
+                dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+            }
+
+            dialog.show()
+        }
+
+        binding.signupRedirectText.setOnClickListener {
             val signupIntent = Intent(this, SignupActivity::class.java)
             startActivity(signupIntent)
-
         }
     }
+
+    private fun compareEmail(email: EditText) {
+        if (email.text.toString().isEmpty()) {
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            return
+        }
+
+        auth.sendPasswordResetEmail(email.text.toString()).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Check your Email", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+
+    private  val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+               if (result.resultCode ==  Activity.RESULT_OK){
+
+                   val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                   handleResults(task)
+               }
+
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>){
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+
+        }else{
+            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent: Intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent) // Start the HomeActivity with the created intent
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
